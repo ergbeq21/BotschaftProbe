ca6ed35f650e0515b4e08a335442b00b7b4b9bdf
Eriseldi <eribeq21@htl-shkoder.com>
2025-03-22 21:50:56 +0100

    Eri commit

--- src/lib/server/db/schema.ts
+++ src/lib/server/db/schema.ts
@@ -1,9 +1,25 @@
 import { mysqlTable, serial, int, varchar, timestamp } from 'drizzle-orm/mysql-core';
 
-
 export const user = mysqlTable('users', {
 	besucher_id: serial('besucher_id').primaryKey(),
 	vorname: varchar('vorname', { length: 50 }).notNull(),
 	nachname: varchar('nachname', { length: 50 }).notNull(),
 	email: varchar('email', { length: 255 }).notNull().unique()
+});
+
+export const allUsers = mysqlTable('allUsers', {
+	id: serial('id').primaryKey(),
+	age: int('age').notNull(),
+	email: varchar('email', { length: 255 }).notNull(),
+	username: varchar('username', { length: 255 }).notNull(),
+	role: varchar('role', { length: 50 }).notNull(),
+	createdAt: timestamp('createdAt').defaultNow(),
+	session: varchar('session', { length: 255 }).notNull(), // Make sure { length: 255 } is always passed
+	sessionExpiration: varchar('sessionExpiration', { length: 255 }).notNull(),
+	password: varchar('password', { length: 255 }).notNull()
+});
+
+export const events = mysqlTable('events', {
+	event_id: serial('event_id').primaryKey(),
+	event_name: varchar('event_name', { length: 255 }).notNull()
 });
